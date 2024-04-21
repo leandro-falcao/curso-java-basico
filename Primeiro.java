135849482dd267922055a98e8f891611583b3786
leandro-falcao <leandrofalcaoti@gmail.com>
2024-04-21 15:24:56 -0300

    first commit

---
+++ Primeiro.java
@@ -0,0 +1,6 @@
+public class Primeiro {
+   public static void main(String[] args) {
+    
+    
+   }
+  }
---
+++ tipos_primitivos.java
@@ -0,0 +1,13 @@
+public class tipos_primitivos {
+
+    public static void main(String[] args) {
+        
+        float nota = 8.5f;
+        float nota2 = 6.6f;
+        
+        float media = (nota + nota2)/2;
+        
+        System.out.println(media);
+    }
+    
+}
